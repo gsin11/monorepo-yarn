6ba954a82a774668512ae44ba26566756d4f423c
Gurpreet Singh <gurpreet.singh7@publicissapient.com>
2020-02-13 16:54:55 +0100

    removed react-app and added jest based test cases

---
+++ packages/Input/lib/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Input = _styledComponents["default"].input.withConfig({
+  displayName: "src__Input",
+  componentId: "sc-1h9q7y1-0"
+})(["background:#fff;color:#000;border-radius:4px;font-size:1rem;font-weight:300;padding:9px 36px;"]);
+
+var _default = Input;
+exports["default"] = _default;
